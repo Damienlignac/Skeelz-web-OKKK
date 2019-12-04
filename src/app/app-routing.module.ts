9675ad13ada3f5fed7faa6f14dc272e3b2cfb4dc
Damien <lignac.damien@gmail.com>
2019-12-04 18:28:12 +0100

    Findall cours

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {ListcoursComponent} from './listcours/listcours.component';
 
 
 
 const routes: Routes = [
-
+  {path: 'cours', component: ListcoursComponent}
 ];
 
 @NgModule({
---
+++ src/app/listcours/listcours.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {ListcoursHttpService} from './listcours.http.service';
+import {Cours} from '../model/cours';
+
+@Component({
+  selector: 'listcours',
+  templateUrl: './listcours.component.html',
+  styleUrls: ['./listcours.component.css']
+})
+export class ListcoursComponent implements OnInit {
+
+  cours: Cours = null;
+
+
+  constructor(private listcoursservice: ListcoursHttpService) {
+  }
+
+  ngOnInit() {
+  }
+
+  list():any {
+    return this.listcoursservice.findAll();
+  }
+
+  add() {
+    this.cours = new Cours();
+    // this.stagiaire.adresse = new Adresse();
+  }
+
+  edit(id: number) {
+    this.listcoursservice.findById(id).subscribe(resp => this.cours = resp);
+    // if(this.stagiaire.adresse == null){
+    //   this.stagiaire.adresse = new Adresse();
+    // }
+  }
+
+  delete(id: number) {
+    this.listcoursservice.deleteBydId(id);
+  }
+
+  cancel() {
+    this.cours = null;
+  }
+
+}
