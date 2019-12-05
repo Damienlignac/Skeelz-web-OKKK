1df7244901f5a4263df8e7bd671b0e3dc7d6846e
Valentin <vapicavet@laposte.net>
2019-12-05 09:42:10 +0100

    edition cours debut

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {ListcoursComponent} from './listcours/listcours.component';
 import {SommaireComponent} from "./sommaire/sommaire.component";
+import {EditionCoursComponent} from './edition-cours/edition-cours.component';
 
 
 
 const routes: Routes = [
   {path: 'cours', component: ListcoursComponent},
-  {path: 'sommaire/:id', component: SommaireComponent}
+  {path: 'sommaire/:id', component: SommaireComponent},
+  {path: 'editionCours', component: EditionCoursComponent}
 ];
 
 @NgModule({
