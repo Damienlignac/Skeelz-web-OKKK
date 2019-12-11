d5b3f64cad95f76b0fc7150ff0e2e3c0297552b3
Damien <lignac.damien@gmail.com>
2019-12-11 09:34:36 +0100

    List 3 meilleurs skeelz

--- src/app/tableau-de-bord/tableau-de-bord.component.ts
+++ src/app/tableau-de-bord/tableau-de-bord.component.ts
@@ -9,6 +9,7 @@ import {AuthService} from '../auth.service';
 import {Personne} from '../model/personne';
 import {Competence} from '../model/competence';
 import {SectionTableauDeBordHttpService} from '../section-tableau-de-bord/section-tableau-de-bord-http-service';
+import {CompetenceSkeelz} from '../model/competenceSkeelz';
 
 @Component({
   selector: 'tableau-de-bord',
@@ -40,14 +41,15 @@ export class TableauDeBordComponent implements OnInit {
   numberponde10: number = 10;
   numberponde15: number = 15;
   numberponde20: number = 20;
+  competenceSkeelz: Array<CompetenceSkeelz>=new Array<CompetenceSkeelz>();
 
-
-
+  skeelz:Skeelz;
 
   skeelznumber1: Skeelz;
   skeelznumber2: Skeelz;
   skeelznumber3: Skeelz;
-
+  mesSkeelzUniques: Array<Skeelz>;
+  listeScoreSkeelz: Array<number>;
   skeelzs: Array<Skeelz> = new Array<Skeelz>();
   comps: Array<Competence> = new Array<Competence>();
   skeelz3s: Array<Skeelz> = new Array<Skeelz>();
@@ -91,53 +93,49 @@ export class TableauDeBordComponent implements OnInit {
 
   }
 
-  // listSkeelz(){
-  //   return this.tableauDeBordHttpService.loadPersonneSkeelz(this.currentPersonne.id);
-  // }
-
-
-  introCours(idCours: number) {
-    this.tableauDeBordHttpService.findIntroCours(idCours, 0).subscribe(resp => {
-      this.module0 = resp;
-      this.router.navigate(['/chapitre/' + [idCours] + '/' + this.module0.id + '/0']);
-    });
-  }
-
-  editionCours(idCours: number) {
-    this.tableauDeBordHttpService.findIntroCours(idCours).subscribe(resp => {
-      this.cours = resp;
-      this.router.navigate(['/editionCours/' + [idCours]]);
-    });
-  }
-
-
   listSkeelz() {
-    console.log("listSkeelzdzadzeafdze")
-    this.sectionTableauDeBordHttpService.loadPersonneSkeelz(this.currentPersonne.id).subscribe(resp => {
+    console.log("listSkeelz")
+    this.tableauDeBordHttpService.loadPersonneSkeelz(this.currentPersonne.id).subscribe(resp => {
       this.skeelzs = resp
+      this.tableauDeBordHttpService.loadPersonneCompetence(this.currentPersonne.id).subscribe(resp => {
+          this.comps = resp
           console.log("list3skeelz")
           console.log(this.skeelzs);
-
+          console.log(this.comps);
           for (let ske of this.skeelzs) { // je recupere mes skeelz
             console.log('je suis ske tour de boucle')
             console.log(ske)
+            for (let comp of this.comps) { // je recupere mes comptence
 
-            for (let compske of (ske.competenceSkeelzs)) { // je recupere mes competenceSkeelz
-              console.log('je suis compske ')
-              console.log(compske)
-
-                for(let comp of compske.competences){ // je recupere mes competences
+              console.log(comp.competenceSkeelz);
+              console.log("je suis comp")
+              console.log(comp);
 
 
+              // for (let compske of (comp.competenceSkeelz)) { // je recupere les skeelz lié a la comp
+              //   console.log("je suis comp.competenceSkeelzs")
+              //   console.log(comp.competenceSkeelz);
+              //   console.log('je suis ske')
+              //   console.log(ske)
+              //   console.log('je suis compske')
+              //   console.log(compske)
+              //   console.log('je suis compske')
+              //   console.log(compske.skeelz)
+
+              for(let skecompske of ske.competenceSkeelz){
+                for(let compskecomp of comp.competenceSkeelz){
+
+
+
+
+                if (skecompske.id == compskecomp.id) {
                   console.log('je suis comp ponde')
                   console.log(comp.ponderation) //mettre valeur ponderation en number
-
                   // @ts-ignore
                   if (comp.ponderation == "CINQ") {
                     this.tmp = this.tmp + this.numberponde5;
-                  }
-                  // @ts-ignore
-                  else if (comp.ponderation == "DIX") {
+                    // @ts-ignore
+                  } else if (comp.ponderation == "DIX") {
                     this.tmp = this.tmp + this.numberponde10;
                     // @ts-ignore
                   } else if (comp.ponderation == "QUINZE") {
@@ -158,37 +156,110 @@ export class TableauDeBordComponent implements OnInit {
                     this.skeelznumber3 = ske;
                   }
                 }
-              }
-
-            this.tmp = 0;
             }
+              }
+            }
+            this.tmp = 0;
+          }
+        }
+      );
+    });
+
+    this.sectionTableauDeBordHttpService.loadPersonneCompetence(this.currentPersonne.id).subscribe(resp =>{
+      this.comps = resp
+      for (let comp of this.comps) {
+        // @ts-ignore
+        if(comp.ponderation == "CINQ"){
+          this.npteGlobal=this.npteGlobal + this.numberponde5;
+        }
+        // @ts-ignore
+        else if(comp.ponderation == "DIX"){
+          this.npteGlobal=this.npteGlobal + this.numberponde10;
+          // @ts-ignore
+        }else if(comp.ponderation == "QUINZE"){
+          this.npteGlobal=this.npteGlobal + this.numberponde15;
+          // @ts-ignore
+        }else if(comp.ponderation == "VINGT"){
+          this.npteGlobal=this.npteGlobal + this.numberponde20;
+        }
+
+      }
+
+    });
 
 
-        });
-    }
 
-//     this.sectionTableauDeBordHttpService.loadPersonneCompetence(this.currentPersonne.id).subscribe(resp => {
-//       this.comps = resp
-//       for (let comp of this.comps) {
-//         // @ts-ignore
-//         if (comp.ponderation == "CINQ") {
-//           this.npteGlobal = this.npteGlobal + this.numberponde5;
-//         }
-//         // @ts-ignore
-//         else if (comp.ponderation == "DIX") {
-//           this.npteGlobal = this.npteGlobal + this.numberponde10;
-//           // @ts-ignore
-//         } else if (comp.ponderation == "QUINZE") {
-//           this.npteGlobal = this.npteGlobal + this.numberponde15;
-//           // @ts-ignore
-//         } else if (comp.ponderation == "VINGT") {
-//           this.npteGlobal = this.npteGlobal + this.numberponde20;
-//         }
-//
-//       }
-//
-//     });
-//
-//   }
-// }
+  }
+
+  introCours(idCours: number) {
+    this.tableauDeBordHttpService.findIntroCours(idCours, 0).subscribe(resp => {
+      this.module0 = resp;
+      this.router.navigate(['/chapitre/' + [idCours] + '/' + this.module0.id + '/0']);
+    });
+  }
+
+  editionCours(idCours: number) {
+    this.tableauDeBordHttpService.findIntroCours(idCours).subscribe(resp => {
+      this.cours = resp;
+      this.router.navigate(['/editionCours/' + [idCours]]);
+    });
+  }
+
+
+  // listSkeelz() { // List de tous les Skeelz (tableau de skeelz dont l'index est similaire a un tableau de nombre (somme des pondération des compétences associé au skeelz)
+  //   console.log("listSkeelzdzadzeafdze")
+  //   this.tableauDeBordHttpService.findCompetenceSkeelzByIdPersonne(this.currentPersonne.id).subscribe(resp => {
+  //     this.competenceSkeelz = resp
+  //     console.log("list3skeelz")
+  //
+  //
+  //     for (let compske of this.competenceSkeelz) { // je recupere mes skeelz
+  //       if ( !this.mesSkeelzUniques.find(item => item.id == compske.skeelz.id)) {
+  //         this.mesSkeelzUniques.push(compske.skeelz);
+  //       }
+  //     }
+  //
+  //     this.listeScoreSkeelz = new Array<number>();
+  //     for(let i: number=0; i < this.mesSkeelzUniques.length; i++) {
+  //       this.listeScoreSkeelz.push(0);
+  //
+  //     }
+  //     console.log((this.listeScoreSkeelz));
+  //     console.log(this.competenceSkeelz);
+  //     for (let compske of this.competenceSkeelz) {
+  //       let index: number;
+  //       let points: number = 0;
+  //       index = this.mesSkeelzUniques.findIndex(item => item.id == compske.skeelz.id);
+  //       console.log(index);
+  //       // @ts-ignore
+  //       if (compske.competence.ponderation == "CINQ") {
+  //         points =  this.numberponde5;
+  //       }
+  //       // @ts-ignore
+  //       else if (compske.competence.ponderation == "DIX") {
+  //         points = this.numberponde10;
+  //         // @ts-ignore
+  //       } else if (compske.competence.ponderation == "QUINZE") {
+  //         points = this.numberponde15;
+  //         // @ts-ignore
+  //       } else if (compske.competence.ponderation == "VINGT") {
+  //         points = this.numberponde20;
+  //       }
+  //       console.log(this.mesSkeelzUniques)
+  //       console.log( this.listeScoreSkeelz)
+  //       this.listeScoreSkeelz[index] += points;
+  //     }
+  //   });
+  // }
+
+
+
+
+
+
+
+
+
+
+
 }
