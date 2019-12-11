48dc9c74a7019ab2e36b19d8b5f6d1cb13b47d37
arthur <arthur.robert18@hotmail.fr>
2019-12-11 19:27:26 +0100

    nop

--- src/app/tableau-de-bord/tableau-de-bord.component.ts
+++ src/app/tableau-de-bord/tableau-de-bord.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ListcoursHttpService} from '../listcours/listcours.http.service';
 import {TableauDeBordHttpService} from './tableau-de-bord-http-service';
 import {Cours} from '../model/cours';
@@ -30,9 +30,9 @@ export class TableauDeBordComponent implements OnInit {
   private coursAdministre: Array<Cours> = new Array<Cours>();
 
   private module0: any;
-  utilisateurRH:boolean=false;
-  boutonMesCoursCompRH:boolean=true;
-  boutonMesemployes:boolean=false;
+  utilisateurRH: boolean = false;
+  boutonMesCoursCompRH: boolean = true;
+  boutonMesemployes: boolean = false;
   number1: number = 0;
   number2: number = 0;
   number3: number = 0;
@@ -45,14 +45,14 @@ export class TableauDeBordComponent implements OnInit {
   numberponde10: number = 10;
   numberponde15: number = 15;
   numberponde20: number = 20;
-  competenceSkeelz: Array<CompetenceSkeelz>=new Array<CompetenceSkeelz>();
-  skeelz:Skeelz;
+  competenceSkeelz: Array<CompetenceSkeelz> = new Array<CompetenceSkeelz>();
+  skeelz: Skeelz;
   mesSkeelzUniques: Array<Skeelz>;
   listeScoreSkeelz: Array<number>;
   skeelzs: Array<Skeelz> = new Array<Skeelz>();
   comps: Array<Competence> = new Array<Competence>();
   skeelz3s: Array<Skeelz> = new Array<Skeelz>();
-  edCours:Cours;
+  edCours: Cours;
 
   //Table Employé
   personne: Personne = null;
@@ -62,17 +62,17 @@ export class TableauDeBordComponent implements OnInit {
   personneSkeelz: Array<Personne> = new Array<Personne>();
 
 
-  constructor(private tableauDeBordHttpService: TableauDeBordHttpService, private sectionTableauDeBordHttpService: SectionTableauDeBordHttpService, private router: Router, public authService: AuthService, private route: ActivatedRoute,private adminUserListService: AdminUserListHttpService) {
+  constructor(private tableauDeBordHttpService: TableauDeBordHttpService, private sectionTableauDeBordHttpService: SectionTableauDeBordHttpService, private router: Router, public authService: AuthService, private route: ActivatedRoute, private adminUserListService: AdminUserListHttpService) {
     this.idUtilisateur = +localStorage.getItem('token');
     this.tableauDeBordHttpService.findByUtilisateur(this.idUtilisateur).subscribe(resp => {
       this.currentPersonne = resp;
-      if(this.currentPersonne.utilisateur.rh == true){
-        this.utilisateurRH= true;
+      if (this.currentPersonne.utilisateur.rh == true) {
+        this.utilisateurRH = true;
       }
 
-      this.listCoursSuivie()
-      this.listCoursTermine()
-      this.listCoursCree()
+      this.listCoursSuivie();
+      this.listCoursTermine();
+      this.listCoursCree();
       this.listSkeelz();
 
     });
@@ -81,29 +81,27 @@ export class TableauDeBordComponent implements OnInit {
 
   ngOnInit() {
   }
+
   //Table Employé
   list(): any {
     this.personnes = this.adminUserListService.findAll();
 
-    console.log(this.personnes)
+    console.log(this.personnes);
     return this.personnes;
   }
 
-  chargeskeelzs(){
+  chargeskeelzs() {
     this.skeelzarray = this.adminUserListService.findAllSkeelz();
     return this.skeelzarray;
   }
 
-  filtreskeelz(){
-    this.adminUserListService.findBySkeelz(this.idSkeelz).subscribe(resp=>{
+  filtreskeelz() {
+    this.adminUserListService.findBySkeelz(this.idSkeelz).subscribe(resp => {
       this.personneSkeelz = resp;
       console.log(this.personneSkeelz);
       return this.personneSkeelz;
     });
   }
-
-
-
 
 
 //Table Cours
@@ -122,81 +120,74 @@ export class TableauDeBordComponent implements OnInit {
   }
 
   listCoursCree() {
-    this.tableauDeBordHttpService.loadCoursCree(this.currentPersonne.id).subscribe(resp => {this.coursAdministre = resp; console.log(this.coursAdministre)});
+    this.tableauDeBordHttpService.loadCoursCree(this.currentPersonne.id).subscribe(resp => {
+      this.coursAdministre = resp;
+      console.log(this.coursAdministre);
+    });
     return
-    ;
+      ;
 
   }
 
   listSkeelz() {
     this.tmp = 0;
     this.number1 = 0;
-    this.number2 =0;
-    this.number3= 0;
-    this.npteGlobal=0;
-    this.skeelznumber1=null
-    this.skeelznumber2=null
-    this.skeelznumber3=null
+    this.number2 = 0;
+    this.number3 = 0;
+    this.npteGlobal = 0;
+    this.skeelznumber1 = null;
+    this.skeelznumber2 = null;
+    this.skeelznumber3 = null;
 
-    console.log("listSkeelz")
+    console.log('listSkeelz');
     this.tableauDeBordHttpService.loadPersonneSkeelz(this.currentPersonne.id).subscribe(resp => {
-      this.skeelzs = resp
+      this.skeelzs = resp;
       this.tableauDeBordHttpService.loadPersonneCompetence(this.currentPersonne.id).subscribe(resp => {
-          this.comps = resp
-          console.log("list3skeelz")
+          this.comps = resp;
+          console.log('list3skeelz');
           console.log(this.skeelzs);
           console.log(this.comps);
           for (let ske of this.skeelzs) { // je recupere mes skeelz
-            console.log('je suis ske tour de boucle')
-            console.log(ske)
-            for (let comp of this.comps) { // je recupere mes comptence
+            for (let skecompske of ske.competenceSkeelz) {
+              console.log('je suis ske tour de boucle');
+              console.log(ske);
+              for (let comp of this.comps) { // je recupere mes comptence
 
-              console.log(comp.competenceSkeelz);
-              console.log("je suis comp")
-              console.log(comp);
+                console.log(comp.competenceSkeelz);
+                console.log('je suis comp');
+                console.log(comp);
 
 
-              // for (let compske of (comp.competenceSkeelz)) { // je recupere les skeelz lié a la comp
-              //   console.log("je suis comp.competenceSkeelzs")
-              //   console.log(comp.competenceSkeelz);
-              //   console.log('je suis ske')
-              //   console.log(ske)
-              //   console.log('je suis compske')
-              //   console.log(compske)
-              //   console.log('je suis compske')
-              //   console.log(compske.skeelz)
-
-              for(let skecompske of ske.competenceSkeelz){
-                for(let compskecomp of comp.competenceSkeelz){
-                if (skecompske.id == compskecomp.id) {
-                  console.log('je suis comp ponde')
-                  console.log(comp.ponderation) //mettre valeur ponderation en number
-                  // @ts-ignore
-                  if (comp.ponderation == "CINQ") {
-                    this.tmp = this.tmp + this.numberponde5;
+                for (let compskecomp of comp.competenceSkeelz) {
+                  if (skecompske.id == compskecomp.id) {
+                    console.log('je suis comp ponde');
+                    console.log(comp.ponderation); //mettre valeur ponderation en number
                     // @ts-ignore
-                  } else if (comp.ponderation == "DIX") {
-                    this.tmp = this.tmp + this.numberponde10;
-                    // @ts-ignore
-                  } else if (comp.ponderation == "QUINZE") {
-                    this.tmp = this.tmp + this.numberponde15;
-                    // @ts-ignore
-                  } else if (comp.ponderation == "VINGT") {
-                    this.tmp = this.tmp + this.numberponde20;
-                  }
+                    if (comp.ponderation == 'CINQ') {
+                      this.tmp = this.tmp + this.numberponde5;
+                      // @ts-ignore
+                    } else if (comp.ponderation == 'DIX') {
+                      this.tmp = this.tmp + this.numberponde10;
+                      // @ts-ignore
+                    } else if (comp.ponderation == 'QUINZE') {
+                      this.tmp = this.tmp + this.numberponde15;
+                      // @ts-ignore
+                    } else if (comp.ponderation == 'VINGT') {
+                      this.tmp = this.tmp + this.numberponde20;
+                    }
 
-                  if (this.number1 < this.tmp) {
-                    this.number1 = this.tmp;
-                    this.skeelznumber1 = ske;
-                  } else if (this.number2 <= this.tmp) {
-                    this.number2 = this.tmp;
-                    this.skeelznumber2 = ske;
-                  } else if (this.number3 <= this.tmp) {
-                    this.number3 = this.tmp;
-                    this.skeelznumber3 = ske;
+                    if (this.number1 < this.tmp) {
+                      this.number1 = this.tmp;
+                      this.skeelznumber1 = ske;
+                    } else if (this.number2 <= this.tmp) {
+                      this.number2 = this.tmp;
+                      this.skeelznumber2 = ske;
+                    } else if (this.number3 <= this.tmp) {
+                      this.number3 = this.tmp;
+                      this.skeelznumber3 = ske;
+                    }
                   }
                 }
-            }
                 this.tmp = 0;
 
               }
@@ -207,28 +198,27 @@ export class TableauDeBordComponent implements OnInit {
       );
     });
 
-    this.sectionTableauDeBordHttpService.loadPersonneCompetence(this.currentPersonne.id).subscribe(resp =>{
-      this.comps = resp
+    this.sectionTableauDeBordHttpService.loadPersonneCompetence(this.currentPersonne.id).subscribe(resp => {
+      this.comps = resp;
       for (let comp of this.comps) {
         // @ts-ignore
-        if(comp.ponderation == "CINQ"){
-          this.npteGlobal=this.npteGlobal + this.numberponde5;
+        if (comp.ponderation == 'CINQ') {
+          this.npteGlobal = this.npteGlobal + this.numberponde5;
         }
         // @ts-ignore
-        else if(comp.ponderation == "DIX"){
-          this.npteGlobal=this.npteGlobal + this.numberponde10;
+        else if (comp.ponderation == 'DIX') {
+          this.npteGlobal = this.npteGlobal + this.numberponde10;
           // @ts-ignore
-        }else if(comp.ponderation == "QUINZE"){
-          this.npteGlobal=this.npteGlobal + this.numberponde15;
+        } else if (comp.ponderation == 'QUINZE') {
+          this.npteGlobal = this.npteGlobal + this.numberponde15;
           // @ts-ignore
-        }else if(comp.ponderation == "VINGT"){
-          this.npteGlobal=this.npteGlobal + this.numberponde20;
+        } else if (comp.ponderation == 'VINGT') {
+          this.npteGlobal = this.npteGlobal + this.numberponde20;
         }
 
       }
 
     });
-
 
 
   }
@@ -241,95 +231,49 @@ export class TableauDeBordComponent implements OnInit {
   }
 
   editionCours(edCours: Cours) {
- // @ts-ignore
-    if ( edCours.etat == "ATTENTE" ){
-      alert("Ce cours est en attente de validation, renseignez-vous auprès de l'administrateur pour ajouter des modifications")
+    // @ts-ignore
+    if (edCours.etat == 'ATTENTE') {
+      alert('Ce cours est en attente de validation, renseignez-vous auprès de l\'administrateur pour ajouter des modifications');
 // @ts-ignore
-     }else if (edCours.etat == "FERME"){
-    this.tableauDeBordHttpService.findIntroCours(edCours.id).subscribe(resp => {
-      this.cours = resp;
+    } else if (edCours.etat == 'FERME') {
+      this.tableauDeBordHttpService.findIntroCours(edCours.id).subscribe(resp => {
+        this.cours = resp;
 
 
-      this.router.navigate(['/editionCours/' + [edCours.id]]);
-    });
-  }else {
+        this.router.navigate(['/editionCours/' + [edCours.id]]);
+      });
+    } else {
       this.introCours(edCours.id);
     }
-}
+  }
+
   //Boutons pour le point de vue RH
 
 
-  boutonMesCoursComp(){
+  boutonMesCoursComp() {
     this.boutonMesCoursCompRH = true;
     this.boutonMesemployes = false;
 
-    this.npteGlobal=0;
-    this.skeelznumber1=null;
-    this.skeelznumber2=null;
-    this.skeelznumber3=null;
-    this.number1=0;
-    this.number2=0;
-    this.number3=0;
+    this.npteGlobal = 0;
+    this.skeelznumber1 = null;
+    this.skeelznumber2 = null;
+    this.skeelznumber3 = null;
+    this.number1 = 0;
+    this.number2 = 0;
+    this.number3 = 0;
 
-    this.listCoursSuivie()
-    this.listCoursTermine()
-    this.listCoursCree()
+    this.listCoursSuivie();
+    this.listCoursTermine();
+    this.listCoursCree();
     this.listSkeelz();
 
   }
-  Boutonemployes(){
+
+  Boutonemployes() {
     this.boutonMesCoursCompRH = false;
     this.boutonMesemployes = true;
 
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   // listSkeelz() { // List de tous les Skeelz (tableau de skeelz dont l'index est similaire a un tableau de nombre (somme des pondération des compétences associé au skeelz)
@@ -377,15 +321,6 @@ export class TableauDeBordComponent implements OnInit {
   //     }
   //   });
   // }
-
-
-
-
-
-
-
-
-
 
 
 }
